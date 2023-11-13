ad01ba6adfd3544034a4257ed0dabc4aacf6e429
marcusvno <marcusnogueira23@gmail.com>
2023-11-13 10:35:38 -0500

    Changed url.

--- iso-packages.sh
+++ iso-packages.sh
@@ -1,9 +1,6 @@
-#/bin/bash
+#!/bin/bash
 
-wget https://github.com/avalon-lake-tech/mrbeast-scripts/blob/main/avalonlake-desktop.png
-
-wget https://cdn.zoom.us/prod/5.16.6.382/zoom_amd64.deb
-
-wget https://downloads.slack-edge.com/releases/linux/4.35.126/prod/x64/slack-desktop-4.35.126-amd64.deb
-
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget "https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/avalonlake-desktop.png"
+wget "https://cdn.zoom.us/prod/5.16.6.382/zoom_amd64.deb"
+wget "https://downloads.slack-edge.com/releases/linux/4.35.126/prod/x64/slack-desktop-4.35.126-amd64.deb"
+wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
