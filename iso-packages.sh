0f5f6ee1d9ea944affc915fb4b3a3babb6a3005a
Marcus Nogueira <112592295+marcusvno@users.noreply.github.com>
2023-11-13 09:41:09 -0500

    Scipt that uses wget to download deb packages for use during the Cubic iso creation

---
+++ iso-packages.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+wget https://github.com/avalon-lake-tech/mrbeast-scripts/blob/main/avalonlake-desktop.png
+
+wget https://cdn.zoom.us/prod/5.16.6.382/zoom_amd64.deb
+
+wget https://downloads.slack-edge.com/releases/linux/4.35.126/prod/x64/slack-desktop-4.35.126-amd64.deb
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
