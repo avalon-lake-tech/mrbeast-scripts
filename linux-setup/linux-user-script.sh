8134c5e365464a3d5da5571cdd43630a9c9aafc3
marcusvno <marcusnogueira23@gmail.com>
2023-11-14 16:18:19 -0500

    Adjustments made for the first time script running on new user.

--- linux-setup/linux-user-script.sh
+++ linux-setup/linux-user-script.sh
@@ -2,13 +2,11 @@
 
 # Ask for the new username
 read -p "Enter the new username: " username
+default_password="solarwinds123" # Set your default password here
 
 # Create new user without sudo permissions
 adduser --disabled-password --gecos "" $username
-
-# Set password for the new user
-echo "Set password for the new user:"
-passwd $username
+echo "$username:$default_password" | chpasswd
 
 # Download and set the background image
 bg_url="https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/avalonlake-desktop.png"
@@ -23,23 +21,17 @@ DISPLAY=:0 GSETTINGS_BACKEND=dconf gsettings set org.gnome.desktop.background pi
 # Set Chrome as the default browser for the new user
 xdg-settings set default-web-browser google-chrome.desktop
 
-# Prompt for password change
-zenity --info --text="Please change your password immediately after logging in for the first time."
+# Configure the dock for the new user
+gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'thunderbird.desktop', 'org.gnome.Nautilus.desktop', 'yelp.desktop', 'org.gnome.Nautilus.desktop trash:///']"
+
+# Prompt for password change and display a welcome message
+zenity --info --text="Welcome to your new account! Please change your password."
 passwd
-
-# Confirmation message to show that the script ran
-zenity --info --text="Initial setup complete. Welcome to your new account!"
-
-# Remove this script
 rm -- "\$0"
 EOL
 
 chmod +x /home/$username/first-login.sh
 echo "/home/$username/first-login.sh" >> /home/$username/.profile
-
-# Modify favorites in the GNOME dock for the new user
-# This step still needs to be done by the current user as it affects the system setting
-gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'yelp.desktop', 'trash:///', 'google-chrome.desktop', 'slack.desktop', 'zoom.desktop']"
 
 # Ensure correct ownership of the files
 chown $username:$username /home/$username/first-login.sh /home/$username/.profile /home/$username/background.jpg
