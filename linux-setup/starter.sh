f4aea11bbdbe5593ca97ee82f49a7044972afe0b
marcusvno <marcusnogueira23@gmail.com>
2023-11-17 12:30:55 -0500

    More adjustments for the demo.

--- linux-setup/starter.sh
+++ linux-setup/starter.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+username=$(whoami)
+
 #Setup background
-wget https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/avalonlake-desktop.png -O /home/$USER/Pictures/background.jpg
-gsettings set org.gnome.desktop.background picture-uri "file:///home/$USER/background.jpg"
+wget https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/avalonlake-desktop.png -O /home/$username/background.jpg
+gsettings set org.gnome.desktop.background picture-uri "file:///home/$username/background.jpg"
 
 #Downloads User script.
 wget https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/linux-setup/linux-user-script.sh
@@ -14,11 +16,11 @@ wget https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/lin
 #Download AnyDesk
 wget https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/linux-setup/anydesk_6.3.0-1_amd64.deb
 
-#Install Veeam
-sudo apt install ./veeam* -y
-sudo apt update -y
-sudo apt install blksnap -y
-sudo apt install veeam -y
+#Install Veeam. Preinstalled for Demo.
+#sudo apt install ./veeam* -y
+#sudo apt update -y
+#sudo apt install blksnap -y
+#sudo apt install veeam -y
 
 #Install AnyDesk
 sudo apt install ./anydesk* -y
@@ -28,8 +30,6 @@ sudo apt install sshfs -y
 
 #Modify the GMD config to remove Wayland display. This is for AnyDesk access. 
 sudo sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
-
-username=$(whoami)
 
 #Setting up the mount sshfs fileserver
 sudo sed -i 's/#user_allow_other/user_allow_other/' /etc/fuse.conf
