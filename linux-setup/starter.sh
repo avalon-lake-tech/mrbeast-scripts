c2f24c52757731151f80b81996ba3a48f7b1cb7a
marcusvno <marcusnogueira23@gmail.com>
2023-11-17 01:24:03 -0500

    Commenting out veeam for the demo.

--- linux-setup/starter.sh
+++ linux-setup/starter.sh
@@ -4,7 +4,9 @@
 wget https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/linux-setup/linux-user-script.sh
 
 #Downloads Veeam Linux Client
-wget https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/linux-setup/veeam-release-deb_1.0.8_amd64.deb
+#wget https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/linux-setup/veeam-release-deb_1.0.8_amd64.deb
+
+#Download AnyDesk
 wget https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/linux-setup/anydesk_6.3.0-1_amd64.deb
 
 #Install Veeam
