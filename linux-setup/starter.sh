429bea581b28ad855626ee29e136ee40fa0fe4c7
marcusvno <marcusnogueira23@gmail.com>
2023-11-16 22:08:07 -0500

    Final touches on the linux starter script.

--- linux-setup/starter.sh
+++ linux-setup/starter.sh
@@ -5,8 +5,10 @@ wget https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/lin
 
 #Downloads Veeam Linux Client
 wget https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/linux-setup/veeam-release-deb_1.0.8_amd64.deb
+wget https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/linux-setup/anydesk_6.3.0-1_amd64.deb
 
 #Install Veeam
-sudo apt install ./veeam*
+sudo apt install ./veeam* -y
 sudo apt install blksnap -y
 sudo apt install veeam
+sudo apt install ./anydesk* -y
