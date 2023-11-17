9af8e0a95d8b617579835375d4c84b51bac981cb
marcusvno <marcusnogueira23@gmail.com>
2023-11-16 23:09:15 -0500

    Mounting the sshfs file.

--- linux-setup/starter.sh
+++ linux-setup/starter.sh
@@ -9,6 +9,22 @@ wget https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/lin
 
 #Install Veeam
 sudo apt install ./veeam* -y
+sudo apt update -y
 sudo apt install blksnap -y
 sudo apt install veeam
+
+#Install AnyDesk
 sudo apt install ./anydesk* -y
+
+#Install sshfs
+sudo apt install sshfs -y
+
+#Modify the GMD config to remove Wayland display. This is for AnyDesk access. 
+sudo sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
+
+#Setting up the mount sshfs fileserver
+sudo sed -i 's/#user_allow_other/user_allow_other/' /etc/fuse.conf
+sudo mkdir -p /mnt/fileserver 
+sudo chown $USER:root /mnt/fileserver
+sudo chmod 770 /mnt/fileserver
+sudo sshfs -o allow_others avalonlackbackups@54.83.52.177:/home/avalonlakebackups/backups /mnt/fileserver
